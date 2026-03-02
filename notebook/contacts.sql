7b44630ef6b6837d61fb9f43cbe03617d4320279
Alexander-Oni <alex.evant1448@gmail.com>
2026-03-02 18:51:54 +0300

    Finished code for contacts table

---
+++ notebook/contacts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE contacts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    last_name   VARCHAR(100) NOT NULL,
+    first_name  VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100) DEFAULT '',
+    gender      ENUM('M','F') DEFAULT NULL,
+    birthdate   DATE DEFAULT NULL,
+    phone       VARCHAR(50) DEFAULT '',
+    address     VARCHAR(255) DEFAULT '',
+    email       VARCHAR(150) DEFAULT '',
+    comment     TEXT,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
